514d1244a3b0403d3c2b35a7496c36c1e454d7f7
avermeulen <andre.vermeulen@gmail.com>
2021-09-06 11:25:52 +0200

    added logic

--- index.js
+++ index.js
@@ -5,8 +5,29 @@ const wordChecker = WordChecker();
 const checkBtn = document.querySelector(".checkBtn");
 const word = document.querySelector(".word");
 const result = document.querySelector(".result");
+const output = document.querySelector(".output");
+const wordsChecked = document.querySelector(".wordsChecked");
+
+let wordCount = 0;
 
 // add behaviour to the elements
 checkBtn.addEventListener("click", function () {
-	result.innerHTML = word.value
+
+/**
+ * 
+`Too short` if the word length is 3 or less in red,
+`Almost there!` if the word length is less 8 or less and longer than 3 in orange,
+`Perfect!` if the number is greater than 8 in green.
+ */
+
+	output.classList.remove('red');
+	output.classList.remove('orange');
+	output.classList.remove('green');
+		
+	wordChecker.check(word.value);
+	
+	result.innerHTML = wordChecker.message()
+	output.classList.add(wordChecker.style());
+	wordsChecked.innerHTML = wordChecker.wordCount();
+
 });
